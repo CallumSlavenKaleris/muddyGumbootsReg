2175b2ce6e10effce6ad8023f4b9af29be2e32ab
Callum Slaven <cslaven@mtn.navis.com>
2024-05-23 10:30:04 +1200

    First Commit

---
+++ src/pages/Login.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import muddyGumbootLogo from "./assets/muddyGumboot.jpg";
+
+const Login = () => {
+  return (
+    <div>
+      <div>
+        <a href="https://www.muddygumbootevents.com/" target="_blank">
+          <img
+            src={muddyGumbootLogo}
+            className="logo"
+            alt="Muddy Gumboots logo"
+          />
+        </a>
+      </div>
+      <h1>Muddy Gumboots</h1>
+      <h2>Max is a cuck</h2>
+    </div>
+  );
+};
+
+export default Login;
