0f4b0958432179c805791b5a6f404d0a67c24ea7
CallumSlavenKaleris <cslaven@mtn.navis.com>
2024-06-10 16:14:15 +1200

    Initial changes to create the inputs for a single registration

--- src/pages/RegistrationSingle.tsx
+++ src/pages/RegistrationSingle.tsx
@@ -1,6 +1,24 @@
 import muddyGumbootLogo from "../assets/muddyGumboot.jpg";
+import React, { useState } from "react";
 
 const RegistrationSingle = () => {
+  interface SignUpFormState {
+    firstname: string;
+    lastname: string;
+    email: string;
+  }
+
+  const [formData, setFormData] = useState<SignUpFormState>({
+    firstname: "",
+    lastname: "",
+    email: "",
+  });
+
+  function Welcome() {
+    console.log(formData.firstname);
+    console.log(formData.lastname);
+  }
+
   return (
     <div>
       <div>
@@ -12,8 +30,45 @@ const RegistrationSingle = () => {
           />
         </a>
       </div>
-      <h1>Muddy Gumboots</h1>
-      <h2>Max is a goose</h2>
+      <div>
+        <input
+          type="text"
+          value={formData.firstname}
+          placeholder="Username"
+          onChange={(e) =>
+            setFormData({
+              firstname: e.target.value,
+              lastname: formData.lastname,
+              email: formData.email,
+            })
+          }
+        />
+        <input
+          type="text"
+          value={formData.lastname}
+          placeholder="lastname"
+          onChange={(e) =>
+            setFormData({
+              firstname: formData.firstname,
+              lastname: e.target.value,
+              email: formData.email,
+            })
+          }
+        />
+        <input
+          type="text"
+          value={formData.email}
+          placeholder="email"
+          onChange={(e) =>
+            setFormData({
+              firstname: formData.firstname,
+              lastname: formData.lastname,
+              email: e.target.value,
+            })
+          }
+        />
+        <button onClick={Welcome}>Register</button>
+      </div>
     </div>
   );
 };
